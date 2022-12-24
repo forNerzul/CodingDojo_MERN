2b4ee2342f78e4d30c0ae02806cce596478789f4
forNerzul <sbeardman92@gmail.com>
2022-12-24 14:15:03 -0300

    Actualizado "5_ordenamiento_rapido_b"

--- js/5_ordenamiento_rapido_b.js
+++ js/5_ordenamiento_rapido_b.js
@@ -1,14 +1,14 @@
 let arr = [4, 8, 5, 7, 1, 6, 10, 2, 3, 9];
 
-function quickSort(arr, lo, hi) {
+function ordenamientoRapido(arr, lo, hi) {
     if (lo >= 0 && hi >= 0 && lo < hi) {
-        const p = partition(arr, lo, hi);
-        quickSort(arr, lo, p);
-        quickSort(arr, p + 1, hi);
+        const p = particion(arr, lo, hi);
+        ordenamientoRapido(arr, lo, p);
+        ordenamientoRapido(arr, p + 1, hi);
     }
 }
 
-function partition(arr, lo, hi) {
+function particion(arr, lo, hi) {
     const pivot = arr[Math.floor((lo + hi) / 2)];
     let i = lo - 1;
     let j = hi + 1;
@@ -25,16 +25,16 @@ function partition(arr, lo, hi) {
             return j;
         }
 
-        swap(arr, i, j);
+        intercambio(arr, i, j);
     }
 }
 
-function swap(arr, i, j) {
+function intercambio(arr, i, j) {
     const tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
 }
 
-quickSort(arr, 0, arr.length - 1);
+ordenamientoRapido(arr, 0, arr.length - 1);
 
 console.log(arr);
